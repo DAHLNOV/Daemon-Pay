fcd77ad06c69c719b38d7f1071219820ba04ff5d
BenjaminWuff <b.paredes@duocuc.cl>
2023-05-17 22:43:39 -0400

    MenuApi Funcional + Models arreglados

--- paysystem/serializer.py
+++ paysystem/serializer.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task, Usuario, Transaccion
 
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ('id', 'title', 'description', 'done') 
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ('correo', 'user', 'contrasena', 'dinero')
+
+class TransaccionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaccion
+        fields = ('user', 'id', 'fecha', 'total', 'estado', 'target') 
--- paysystem/views.py
+++ paysystem/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from .serializer import TaskSerializer
-from .models import Task
+from .serializer import TaskSerializer, UsuarioSerializer, TransaccionSerializer
+from .models import Task, Usuario, Transaccion
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
@@ -8,6 +8,14 @@ from django.shortcuts import render, HttpResponse
 class TaskView(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+
+class UsuarioView(viewsets.ModelViewSet):
+    serializer_class = UsuarioSerializer
+    queryset = Usuario.objects.all()
+
+class TransaccionView(viewsets.ModelViewSet):
+    serializer_class = TransaccionSerializer
+    queryset = Transaccion.objects.all()
 
 
 def homeApi(request):
