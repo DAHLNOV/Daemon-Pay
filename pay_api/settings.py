390f87001f7c99ae34aad0e618b0b7327028317e
DAHLNOV <jdahlbokum@gmail.com>
2023-06-23 14:47:34 -0400

    Docs Fix

--- pay_api/settings.py
+++ pay_api/settings.py
@@ -132,4 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+}
 
